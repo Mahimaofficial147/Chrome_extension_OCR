bef52e9bf403d24c24aceebf73961384e0ce2e91
Mahima Kumari <121475295+Mahimaofficial147@users.noreply.github.com>
2024-01-03 22:15:11 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,8 @@
+﻿document.getElementById("submit").onclick = async () => {
+  const url = document.getElementById("url").value;
+
+  const res = await fetch(url);
+  const blob = await res.blob();
+
+  iframe.contentWindow.postMessage(blob, "*");
+}
